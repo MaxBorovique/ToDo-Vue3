d70a6c9777c2b0dd317ea9df77a88a5f2f190bbb
Maksym <max.borovique@gmail.com>
2024-10-17 22:47:15 +0300

    j

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'',
+  base: '/ToDo-Vue3/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
 })
